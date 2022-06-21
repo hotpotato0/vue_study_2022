0a0653d495ac987e31856a73b6bc4bb6b586c607
ghdehddnr92 <ghdehddnr92@gmail.com>
2022-06-21 23:11:28 +0900

    vue-cli 강의

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- -->
+    <!-- <hello-world></hello-world>
+    <HelloWorld></HelloWorld>
+    <HelloWorld/> -->
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
+  name: 'App',
+  components: {
+    HelloWorld,
+    // 'hello-world': HelloWorld,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
