98711a8d62681ec9dc98e09aa421b1c72efb042a
윤상돈 <yunsangdon@Donys-MacBookPro.local>
2022-06-28 19:47:02 +0900

    초급 종료

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,18 @@
+<template>
+<!--HTML-->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+// javascript
+  methods: {
+    addNum: function{
+    }
+  }
+}
+</script>
+
+<style>
+/*css*/
+</style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <!--HTML-->
+  <header>
+    <h1>{{propsdata}}</h1>
+    <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+// javascript
+  props: ['propsdata'],
+  methods: {
+    sendEvent: function(){
+      this.$emit('renew')
+    }
+  }
+}
+</script>
+
+<style>
+/*css*/
+</style>
