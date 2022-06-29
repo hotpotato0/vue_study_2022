13bb8194a99f9aa67d8a9a2b9c563f8a95c562ec
윤상돈 <yunsangdon@Donys-MacBookPro.local>
2022-06-30 00:46:28 +0900

    todo app 기본 수강 완료

---
+++ vue-intermediate/vue-todo/src/components/TodoList.vue
@@ -0,0 +1,97 @@
+<template>
+<div>
+  <ul>
+    <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
+      <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)">
+      </i>
+      <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
+
+      <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+        <i class="fas fa-trash" ></i>
+      </span>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  data: function (){
+    return {
+      todoItems: []
+    }
+  },methods: {
+    removeTodo: function (todoItem, index) {
+      // console.log(todoItem);
+      // console.log(index);
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index,1);
+      // this.todoItems.pop(this.todoItems);
+    },
+    toggleComplete: function (todoItem){
+      // console.log(todoItem, index);
+      todoItem.completed = !todoItem.completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
+    }
+  },
+  created: function () {
+    // console.log('created');
+    if(localStorage.length > 0){
+      for(var i = 0 ; i < localStorage.length ; i++){
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server')
+          // console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          // localStorage.getItem(localStorage.key(i));
+        // console.log(localStorage.key(i));
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+
+/* transition css */
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+</style>
